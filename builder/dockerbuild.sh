f54fbdfa1dea50545c168a619340df8644da2e95
Jason D. McCormick <jason@mfamily.org>
2024-03-19 17:49:11 -0400

    initial fork of build runners

---
+++ builder/dockerbuild.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -e
+set -x
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -c|--check-changelog)
+      CHECK_CHANGELOG=YES
+      shift
+      ;;
+    -a|--architecture)
+      ARCH="$2"
+      shift
+      shift
+      ;;
+    -t|--targets)
+      TARGETS="$2"
+      shift
+      shift
+      ;;
+    -r|--commit-versioning)
+      COMMIT_VERSIONING=YES
+      shift
+      ;;
+    -o|--operating-systems)
+      OPERATING_SYSTEMS="$2"
+      shift
+      shift
+      ;;
+    -*|--*|*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$ARCH" ]
+then
+  ARCH="all"
+fi
+
+if [ -z "$TARGETS" ]
+then
+  TARGETS="Allmon3"
+fi
+
+if [ -z "$OPERATING_SYSTEMS" ]
+then
+  OPERATING_SYSTEMS="buster"
+fi
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $BRANCH == "develop" ]; then
+  REPO_ENV="-devel"
+elif [ $BRANCH = "testing"]; then
+  REPO_ENV="-testing"
+else
+  REPO_ENV=""
+fi
+
+echo "Architectures: $ARCH"
+echo "Targets: $TARGETS"
+echo "Operating Systems: $OPERATING_SYSTEMS"
+echo "PWD: $(pwd)"
+echo "BS: ${BASH_SOURCE[0]}"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "DIR: ${DIR}"
+PDIR=$(dirname $DIR)
+echo "PDIR: ${PDIR}"
+
+DPKG_BUILDOPTS="-b -uc -us"
+D_TAG="asl3-asterisk_builder.${OPERATING_SYSTEMS}.${ARCH}${REPO_ENV}"
+
+docker build -f $DIR/Dockerfile -t $D_TAG \
+	--build-arg ARCH="$ARCH" \
+	--build-arg OS="$OPERATING_SYSTEMS" \
+	--build-arg ASL_REPO="asl_builds${REPO_ENV}" \
+	--build-arg USER_ID=$(id -u) \
+	--build-arg GROUP_ID=$(id -g) \
+	$DIR
+
+docker run -v $PDIR:/build/Allmon3 \
+	-e DPKG_BUILDOPTS="$DPKG_BUILDOPTS" \
+	-e BUILD_TARGETS="$TARGETS" \
+	$D_TAG
+
+docker image rm --force $D_TAG
---
+++ builder/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+#get DPKG_BUILDOPTS from env var or use default
+OPTS=${DPKG_BUILDOPTS:-"-b -uc -us"}
+
+if [ -f /etc/os-release ] ; then
+  OS_CODENAME=$(cat /etc/os-release | grep "^VERSION_CODENAME=" | sed 's/VERSION_CODENAME=\(.*\)/\1/g')
+elif [ command -v lsb_release ] ; then
+  OS_CODENAME=$(lsb_release -a 2>/dev/null | grep "^Codename:" | sed 's/^Codename:\s*\(.*\)/\1/g')
+elif [ command -v hostnamectl ] ; then
+  OS_CODENAME=$(hostnamectl | grep "Operating System: " | sed 's/.*Operating System: [^(]*(\([^)]*\))/\1/g')
+else
+  OS_CODENAME=unknown
+fi
+
+echo "OS_CODENAME: ${OS_CODENAME}"
+cd /build/Allmon3
+
+case $OS_CODENAME in
+	bullseye)
+		echo 11 > debian/compat
+	;;
+	bookworm)
+		echo 12 > debian/compat
+	;;
+	*)
+		echo 13 > debian/compat
+	;;
+esac
+
+export EMAIL="AllStarLink <autobuild@allstarlink.org>"
+make docker-deb DPKG_BUILDOPTS="${OPTS}" RELPLAT=$OS_CODENAME
+[ ! -d _debs ] && mkdir _debs
+rm -f _debs/*
+cp ../*.deb _debs/
